f85d3f62d1c42879234ef2f1d6555c23eb153e69
Bas Stottelaar <basstottelaar@gmail.com>
2022-08-08 21:55:05 +0200

    Fix incorrect is_deleted check

--- aiobiketrax/client.py
+++ aiobiketrax/client.py
@@ -236,7 +236,7 @@ class Device:
 
     @property
     def is_deleted(self) -> bool:
-        return self._id in self._device
+        return self._device is not None
 
     @property
     def is_alarm_triggered(self) -> Optional[bool]:
