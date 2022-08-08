fba00c12ece6ced100863dddaae910176a99b1fd
Bas Stottelaar <basstottelaar@gmail.com>
2022-08-08 23:54:35 +0200

    Add CLI command to retrieve trips for device

--- aiobiketrax/cli.py
+++ aiobiketrax/cli.py
@@ -5,7 +5,7 @@ import sys
 
 import aiohttp
 
-from .client import Account, Device
+from .client import Account, Device, Trip
 
 
 def parse_arguments(argv: list[str]) -> argparse.Namespace:
@@ -20,9 +20,19 @@ def parse_arguments(argv: list[str]) -> argparse.Namespace:
 
     sub_parsers = parser.add_subparsers(dest="command", help="command to execute")
 
-    sub_parsers.add_parser("devices", help="display all devices")
+    _ = sub_parsers.add_parser("devices", help="display all devices")
 
-    sub_parsers.add_parser("stream", help="stream device updates")
+    _ = sub_parsers.add_parser("stream", help="stream device updates")
+
+    trips_parser = sub_parsers.add_parser("trips", help="display all devices")
+
+    trips_parser.add_argument(
+        "-d",
+        "--device-id",
+        action="store",
+        required=True,
+        help="device identifier to retrive trips of",
+    )
 
     # Parse command line
     return parser.parse_args(argv[1:])
@@ -77,16 +87,53 @@ async def command_stream(arguments: argparse.Namespace):
             await account.stop()
 
 
+async def command_trips(arguments: argparse.Namespace):
+    async with aiohttp.ClientSession() as session:
+        account = Account(
+            username=arguments.username, password=arguments.password, session=session
+        )
+
+        await account.update_devices()
+
+        devices = list(
+            filter(lambda d: str(d.id) == arguments.device_id, account.devices)
+        )
+
+        if not devices:
+            sys.stdout.write(
+                f"Device with identifier {arguments.device_id} not found.\n"
+            )
+            return
+
+        device = devices[0]
+
+        await device.update_trips()
+
+        for trip in device.trips:
+            sys.stdout.write(pprint.pformat(trip_to_dict(trip)) + "\n")
+
+
 def device_to_dict(device: Device) -> dict[str, any]:
-    """Lazy-man's approach to convert a `Device` to a `dict`."""
+    return instance_to_dict(device, ["is_deleted", "trips"])
+
+
+def trip_to_dict(trip: Trip) -> dict[str, any]:
+    return instance_to_dict(trip, ["is_deleted", "key"])
+
+
+def instance_to_dict(instance: object, exclude: list[str]) -> dict[str, any]:
+    """Lazy-man's approach to convert an `object` to a `dict`."""
 
     result = {}
 
-    for key in dir(device):
+    for key in dir(instance):
         if key.startswith("_"):
             continue
 
-        attr = getattr(device, key)
+        if key in exclude:
+            continue
+
+        attr = getattr(instance, key)
 
         if callable(attr):
             continue
@@ -108,6 +155,8 @@ async def main(argv: list[str]) -> int:
         await command_devices(arguments)
     elif arguments.command == "stream":
         await command_stream(arguments)
+    elif arguments.command == "trips":
+        await command_trips(arguments)
 
     return 0
 
